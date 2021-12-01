b602fc8021aef6cb70cfe71e6ac52a4fd138342a
jsyeh <jsyeh@mail.mcu.edu.tw>
2021-12-01 11:56:42 +0800

    這是我第一次用git指令commit

--- week03-2.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()           ///新的一行,跳行
-{             /// 反斜線\n 代表 new line
-    printf("Hello World\n");
-    printf("Hello World\n");
-}
--- week03-3.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()           ///新的一行,跳行
-{             /// 反斜線\n 代表 new line
-    printf("Hello World\n");
-    printf("%d\n", 300 );
-    printf("%d %d  %d\n, 300, 200, 100");
-} ///%d decimal digit 10進位的整數
